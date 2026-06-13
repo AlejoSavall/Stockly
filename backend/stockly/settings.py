c076eea06498d97cb602ba99bd0996572cefb1bc
AlejoSavall <thealejohd1345@gmail.com>
2026-06-13 13:19:55 -0300

    feat: Update requirements.txt

--- backend/stockly/settings.py
+++ backend/stockly/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'sales',
     'expenses',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'stockly.urls'
@@ -128,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
